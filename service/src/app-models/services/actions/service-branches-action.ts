1632407e7ab6c23381a10e53fa4a4c48c4ce2a4c
Gallay Lajos <gallay.lajos@gmail.com>
2026-05-07 10:08:26 +0200

    fix: Minor usability fixes

--- service/src/app-models/services/actions/service-branches-action.ts
+++ service/src/app-models/services/actions/service-branches-action.ts
@@ -2,6 +2,8 @@ import { RequestError } from '@furystack/rest'
 import { JsonResult, type RequestAction } from '@furystack/rest-service'
 import type { ServiceBranchesEndpoint } from 'common'
 import { ServiceDefinition, ServiceStatus } from 'common'
+import { existsSync } from 'fs'
+import { join } from 'path'
 import { GitService } from '../../../services/git-service.js'
 import { resolveServiceCwd } from '../../../utils/resolve-service-cwd.js'
 import { legacyRepository as getRepository } from '../../../utils/legacy-repository.js'
@@ -20,11 +22,22 @@ export const ServiceBranchesAction: RequestAction<ServiceBranchesEndpoint> = asy
     .getDataSetFor(ServiceStatus, 'serviceId')
     .find(injector, { filter: { serviceId: { $eq: serviceId } }, top: 1 })
   const status = statuses[0]
-  if (!status || status.cloneStatus !== 'cloned') {
+  const cloneStatus = status?.cloneStatus
+
+  if (cloneStatus !== 'cloned' && cloneStatus !== 'cloning') {
     throw new RequestError('Repository is not cloned yet', 400)
   }
 
   const cwd = await resolveServiceCwd(injector, svc, injector)
+
+  // Allow reads while a pull/refresh is mid-flight on an already-cloned repo;
+  // git ref scans are read-only and safe to run concurrently with `git fetch`.
+  // For the `cloning` state, require a `.git` directory on disk so we don't
+  // race the initial clone.
+  if (cloneStatus === 'cloning' && !existsSync(join(cwd, '.git'))) {
+    throw new RequestError('Repository is not cloned yet', 400)
+  }
+
   const git = injector.get(GitService)
 
   const [currentBranch, branches] = await Promise.all([git.getCurrentBranch(cwd), git.getBranches(cwd)])
